9d5a71c88a17885e32c90ec5dc30aee4af6a5ce6
Eruiz2 <esteban.ruiz2@globallogic.com>
2024-12-03 12:12:20 -0300

    asdasd

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@
 
    tags = {                                
      Name = "demo-server1"
-}
+          }
 }
 
 
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
-  ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
+  ami           = "ami-038bba9a164eb3dc1"  # AMI for us-west-1
   instance_type = "t2.micro"
 
   tags = {                                
